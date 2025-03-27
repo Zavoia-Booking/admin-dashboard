af27d300972d762a6f799d21d1d397e085cb1141
Andrei Sandica <andrei.sandica.94@gmail.com>
2025-03-27 20:00:44 +0200

    no need for transpile yet

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  
-  // Add transpilePackages to handle MUI and other package imports
-  transpilePackages: [
-    '@mui/material',
-    '@mui/icons-material',
-    '@mui/x-date-pickers',
-    '@aldabil/react-scheduler',
-  ],
 };
 
 export default nextConfig;
